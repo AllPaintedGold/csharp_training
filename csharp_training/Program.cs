6e8a1a1dfe6f6ffed11c179226d2a01f32a84a2d
unknown <johannesfreutel@gmail.com>
2021-12-22 17:04:46 +0100

    challange String Conversion to Hex

--- csharp_training/Program.cs
+++ csharp_training/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace csharp_training
 {
@@ -6,7 +7,39 @@ namespace csharp_training
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Create a function that takes a string's characters
+            // as ASCII and returns each character's
+            // hexadecimal value as a string.
+            // hex characters must be lowercase
+            // each byte must be seperated by a space
+
+            string huhu = "ich bins nur";
+
+            Console.WriteLine(stringIntoHex(huhu));
+
+            Console.ReadKey();
+        }
+
+        protected static string stringIntoHex(string huch)
+        {
+            // ASCII for each char
+            // from ASCII representation to HEX
+            // ASCII value / 16 = first digit
+            // remaining = second digit ( in hex notation ) 
+            // hex notation being : 0 1 2 3 4 5 6 7 8 9 A B C D E F 
+            // 16 possible values
+
+            string finalValue = null;
+            int value;
+
+            foreach(char c in huch)
+            {
+                value = Convert.ToInt32(c);
+                finalValue = String.Format("{0} {1:X}", finalValue, value);
+            }
+           
+
+            return finalValue.TrimEnd().ToLower();
         }
     }
 }
