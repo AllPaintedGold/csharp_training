4ec5414f821937c0b7772cb75e09c3c91c097a41
unknown <johannesfreutel@gmail.com>
2021-12-22 09:16:52 +0100

    initial commit

---
+++ csharp_training/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace csharp_training
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
